1115bbd3c5d0a68a83619ca926fd97d1bf82b059
pichaachuu <nafisahaliyah01@gmail.com>
2024-10-15 09:15:09 +0700

    Perulangan For

--- SiakadFor13.java
+++ SiakadFor13.java
@@ -4,7 +4,8 @@ public class SiakadFor13 {
     public static void main(String[] args) {
         Scanner input13 = new Scanner(System.in);
 
-        double nilai, tertinggi = 0, terendah = 100;
+        double nilai, tertinggi = 100, terendah = 0, batasLulus = 60;
+        int lulus = 0, tidakLulus = 0;
 
         for (int i = 1; i <= 10; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
@@ -15,10 +16,18 @@ public class SiakadFor13 {
             if (nilai < terendah) {
                 terendah = nilai;
             }
+
+            if (nilai > 60) {
+                lulus ++;
+            } else {
+                tidakLulus ++;
+            }
         }
 
         System.out.println("Nilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);
+        System.out.println("Jumlah yang lulus adalah: " + lulus);
+        System.out.println("Jumlah yang tidak lulus adalah: " + tidakLulus);
 
 
     }
